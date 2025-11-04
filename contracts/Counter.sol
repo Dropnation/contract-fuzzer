09b0eb58549b2e6fa0c4e0bbb421301b4995d302
ImKedio <martingniazdowski@gmail.com>
2025-11-04 11:16:17 +0100

    Contract

---
+++ contracts/Counter.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Counter {
+    int256 public value;
+
+    function inc(int256 x) external {
+        value += x;
+    }
+
+    function dec(int256 x) external {
+        value -= x;
+    }
+
+    function echidna_never_negative() public returns (bool) {
+        return value >= 0;
+    }
+}
+
+
